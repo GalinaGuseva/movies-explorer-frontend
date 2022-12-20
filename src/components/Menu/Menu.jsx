33549e843cb7762a9b8c699fc03e633a7a3f6c51
Galina Guseva <galaguss@yandex.ru>
2022-12-20 12:52:53 +0500

    fix: react scripts

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,48 +1,53 @@
-import React from 'react';
-import './Menu.css';
-import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import "./Menu.css";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Menu({ isOpen, onClose }) {
   return (
-    isOpen && (
-      <section className="menu" onClick={onClose}>
-        <div className="menu__container">
-          <div className="menu__bar">
-            <button onClick={onClose} className="menu__btn-close link"></button>
-            <nav className="menu__nav">
-              <NavLink to="/" onClick={onClose} className="menu__link link">
-                Главная
-              </NavLink>
-              <NavLink
-                to="/movies"
-                className={({ isActive }) =>
-                  isActive
-                    ? 'menu__link menu__link_active-movies'
-                    : 'menu__link link'
-                }
-              >
-                Фильмы
-              </NavLink>
-              <NavLink
-                onClick={onClose}
-                to="/saved-movies"
-                className={({ isActive }) =>
-                  isActive ? 'menu__link menu__link_active' : 'menu__link link'
-                }
-              >
-                Сохранённые фильмы
-              </NavLink>
-            </nav>
-          </div>
-          <Link
-            to="/profile"
-            className="menu__profile-link link"
-            onClick={onClose}
-          >
-            Аккаунт
-          </Link>
+    <section
+      className={`menu ${isOpen ? "menu_active" : ""}`}
+      onClick={onClose}
+    >
+      <div
+        className={`menu__container ${
+          isOpen ? "menu__container_visible" : "menu__container_hidden"
+        }`}
+      >
+        <div className="menu__bar">
+          <button onClick={onClose} className="menu__btn-close link"></button>
+          <nav className="menu__nav">
+            <NavLink to="/" onClick={onClose} className="menu__link link">
+              Главная
+            </NavLink>
+            <NavLink
+              to="/movies"
+              className={({ isActive }) =>
+                isActive
+                  ? "menu__link menu__link_active-movies"
+                  : "menu__link link"
+              }
+            >
+              Фильмы
+            </NavLink>
+            <NavLink
+              onClick={onClose}
+              to="/saved-movies"
+              className={({ isActive }) =>
+                isActive ? "menu__link menu__link_active" : "menu__link link"
+              }
+            >
+              Сохранённые фильмы
+            </NavLink>
+          </nav>
         </div>
-      </section>
-    )
+        <Link
+          to="/profile"
+          className="menu__profile-link link"
+          onClick={onClose}
+        >
+          Аккаунт
+        </Link>
+      </div>
+    </section>
   );
 }
